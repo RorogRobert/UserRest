6a7c8b2a5f8d3dac15310f54d8833bd16a0c30f2
Robert Rorog <r.rorog@innovagile.com>
2020-02-05 19:42:05 +0100

    added orm-pack bundle, updated entity User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,11 +2,8 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -22,8 +19,6 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Assert\NotBlank()
-     * @Assert\Email()
      */
     private $email;
 
